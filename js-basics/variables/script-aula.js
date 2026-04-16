d1a5f69c8beb6d8b48864517e33699cf964a3a2b
jrs-neto <jrd.santosneto@gmail.com>
2026-04-16 16:53:36 -0300

    first commit

---
+++ js-basics/variables/script-aula.js
@@ -0,0 +1,35 @@
+let nome = "José";
+let idade = 32;
+let possuiFaculdade = true;
+
+console.log(nome, idade, possuiFaculdade);
+
+let preco = 25;
+let totalComprado = 5;
+let totalPreco = totalComprado * preco;
+
+console.log(totalPreco);
+
+let sobrenome = "Rafael",
+  cidade = "Rio";
+
+console.log(sobrenome, cidade);
+
+let semDefinir;
+console.log(semDefinir);
+
+// console.log(aindaNaoDefine);
+
+let comida;
+console.log(comida);
+comida = "Pizza";
+console.log(comida);
+
+let time = "Vasco";
+// console.log(time);
+
+time = "Flamengo";
+console.log(time);
+
+// const time = "Vasco";
+// time = "Fluminese";
---
+++ js-basics/variables/script.js
@@ -0,0 +1,15 @@
+// Declarar uma variável com o seu nome
+let nome = "José";
+
+// Declarar uma variável com a sua idade
+let idade = 32;
+
+// Declarar uma variável com a sua comida
+// favorita e não atribuir valor
+let comidaFavorita;
+
+// Atribuir valor a sua comida favorita
+comidaFavorita = "Lasanha";
+
+// Declarar 5 variáveis diferentes sem valores
+let sobrenome, cidade, gamer, possuiFaculdade, cep;
