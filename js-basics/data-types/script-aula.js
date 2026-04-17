3d1349301cfecba854ec62d505285fbd55af55d6
jrs-neto <jrd.santosneto@gmail.com>
2026-04-17 14:37:29 -0300

    study: data types lesson and exercises

---
+++ js-basics/data-types/script-aula.js
@@ -0,0 +1,33 @@
+// let nome = "José";
+let idade = 32;
+// let frase;
+let simbolo = Symbol();
+
+// console.log(typeof nome);
+console.log(typeof idade);
+// console.log(typeof frase);
+console.log(typeof simbolo);
+
+let nome = "Andre";
+let sobrenome = "Rafael";
+let nomeCompleto = nome + " " + sobrenome;
+
+console.log(nomeCompleto);
+
+// let gols = 1000;
+// let frase = "Romário fez " + gols + " gols";
+// console.log(frase);
+// console.log(typeof frase); // Ao somar uma frase com um número o retorno é uma string
+
+let ano = "2018";
+let mes = 8;
+console.log(ano + mes); // concatenação
+
+let palavraTeste = "teste";
+let frase1 = 'Esse é o \"melhor\" jogo';
+console.log(frase1);
+
+let gols = 1000;
+let frase2 = `Romário fez ${gols * 2} gols`;
+
+console.log(frase2);
---
+++ js-basics/data-types/script.js
@@ -0,0 +1,24 @@
+// Declare uma variável contendo uma string
+let frase = "José Rodrigues - Full Stack Developer";
+console.log(frase);
+
+// Declare uma variável contendo um número dentro de uma string
+let numeroString = "50";
+console.log(numeroString);
+
+// Declare uma variável com a sua idade
+let idade = 32;
+console.log(idade);
+
+// Declare duas variáveis, uma com seu nome
+// e outra com seu sobrenome e some as mesmas
+let nome = "José";
+let sobrenome = "Rodrigues";
+console.log(`Meu nome é ${nome} ${sobrenome}`);
+
+// Coloque a seguinte frase em uma variável: It's time
+let frase2 = "It\'s time";
+console.log(frase2);
+
+// Verifique o tipo da variável que contém o seu nome
+console.log(typeof nome);
